3e2f69a10ee7f283f74b7a8b76abee5582ff2a4a
GrandpaRex <77104671+GrandpaRex@users.noreply.github.com>
2023-06-01 21:35:02 -0500

    Add files via upload

---
+++ onDutyExport/onDutyExport/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using FivePD.API;
+using FivePD.API.Utils;
+
+namespace onDutyExport
+{
+    public class Main : Plugin
+    {
+        internal Main() 
+        {
+            EventHandlers.Add("fivepd::dutyStatus", new Action<CallbackDelegate>((cb) => GetData(cb)));
+        }
+
+        private void GetData(CallbackDelegate cb)
+        {
+            bool dutyStatus = Utilities.IsPlayerOnDuty();
+            cb.Invoke(dutyStatus);
+        }
+    }
+}
